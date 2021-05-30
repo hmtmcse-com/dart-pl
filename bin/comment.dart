71af1c0388f1118377c833e019aeef918cc368f7
hmtmcse <hmtm.cse@gmail.com>
2021-05-30 06:17:50 +0600

    dart programming

---
+++ bin/comment.dart
@@ -0,0 +1,13 @@
+
+// print is responsible for write/show something into console
+void main(List<String> arguments) {
+  print("This is Bismillah Program"); // We can add comment in same line of code execution
+}
+
+
+void otherClass() {
+  /*
+  Actually this is multi-line comments
+  We can add multiple line here
+   */
+}
---
+++ bin/data_holder.dart
@@ -0,0 +1,37 @@
+void main() {
+  var stringData = 'This is String Data';
+  var integerData = 100;
+  var floatData = 100.10;
+  var booleanData = true;
+}
+
+
+void variableWithDataTypes() {
+  String stringData = 'This is String Data';
+  int integerData = 100;
+  double floatData = 100.10;
+  bool booleanData = true;
+}
+
+void dataTypeExample() {
+  var stringData = "This is 'String' Data";
+  var stringDataOther = 'This is also String Data';
+  String stringDataWithType = 'This is String Data with data type';
+
+  var integerData = 100;
+  int integerDataWithType = 100;
+
+  var doubleData = 100.10;
+  double doubleDtaWithType = 100.10;
+
+  var booleanData = true;
+  bool booleanDataWithType = true;
+
+  final PI = 3.41; // Without a type annotation
+  final double PI_WITH_TYPE = 3.41;
+
+  const bar = 1000000; // Unit of pressure (dynes/cm2)
+  const double atm = 1.01325 * bar; // Standard atmosphere
+}
+
+
